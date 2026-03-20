020b8af4c3534c6a835ca9391bf90b3dc841c6be
Your Name <youremail@example.com>
2026-03-20 18:24:50 +0100

    cxzcz

--- kaka/dllmain.cpp
+++ kaka/dllmain.cpp
@@ -13,6 +13,11 @@ bool kPressed = false;
 bool bPressed = false;
 bool fPressed = false;
 
+// Petite fonction de XOR pour masquer les chaînes
+void xor_string(char* data, size_t size, char key) {
+    for (size_t i = 0; i < size; i++) data[i] ^= key;
+}
+
 DWORD WINAPI MainThread(LPVOID lpParam) {
     HMODULE hModule = (HMODULE)lpParam;
 
@@ -21,15 +26,20 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
         return 0;
     }
 
-    HMODULE jvmHandle = GetModuleHandleA("jvm.dll");
+    char jvm_name[] = { 0x3f, 0x23, 0x38, 0x7b, 0x31, 0x39, 0x39, 0x00 };
+    xor_string(jvm_name, 7, 0x55);
+    HMODULE jvmHandle = GetModuleHandleA(jvm_name);
     if (!jvmHandle) {
         MH_Uninitialize();
         FreeLibraryAndExitThread(hModule, 0);
         return 0;
     }
 
+    char proc_name[] = { 0x1f, 0x1b, 0x1c, 0x0a, 0x12, 0x30, 0x21, 0x36, 0x37, 0x34, 0x31, 0x30, 0x31, 0x31, 0x1f, 0x34, 0x23, 0x34, 0x03, 0x18, 0x26, 0x00 };
+    xor_string(proc_name, 21, 0x55);
+
     typedef jint(JNICALL* GetCreatedJavaVMs_t)(JavaVM**, jsize, jsize*);
-    auto getVMs = (GetCreatedJavaVMs_t)GetProcAddress(jvmHandle, "JNI_GetCreatedJavaVMs");
+    auto getVMs = (GetCreatedJavaVMs_t)GetProcAddress(jvmHandle, proc_name);
     getVMs(&g_vm, 1, nullptr);
 
     if (g_vm && g_vm->AttachCurrentThread((void**)&g_env, nullptr) == JNI_OK) {
