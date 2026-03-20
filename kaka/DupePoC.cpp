6ee9b030f733b035db4d676c41c4ea33da4e4a5c
Your Name <youremail@example.com>
2026-03-20 17:01:03 +0100

    dsda

--- kaka/DupePoC.cpp
+++ kaka/DupePoC.cpp
@@ -2,52 +2,48 @@
 #include <string>
 #include <fstream>
 
-void Step(const char* msg) {
-    std::ofstream log("kaka_step_log.txt", std::ios::app);
-    log << "Etape: " << msg << "\n";
-}
+
 
 void DupePoC::Run(JNIEnv* env, int gunSlot, int attachSlot) {
-    Step("Init");
+
     if (!env) return;
 
-    Step("FindClass Thread");
+
     jclass threadClass = env->FindClass("java/lang/Thread");
     if (!threadClass) { env->ExceptionClear(); return; }
 
-    Step("Get getAllStackTraces");
     jmethodID getAllStackTracesMid = env->GetStaticMethodID(threadClass, "getAllStackTraces", "()Ljava/util/Map;");
     if (!getAllStackTracesMid) return;
 
-    Step("Call getAllStackTraces");
+
     jobject map = env->CallStaticObjectMethod(threadClass, getAllStackTracesMid);
     if (!map) return;
 
-    Step("Get Map Class");
+
     jclass mapClass = env->GetObjectClass(map);
     if (!mapClass) return;
 
-    Step("Get keySet Method");
+
     jmethodID keySetMid = env->GetMethodID(mapClass, "keySet", "()Ljava/util/Set;");
     if (!keySetMid) return;
 
-    Step("Call keySet");
+
     jobject set = env->CallObjectMethod(map, keySetMid);
     if (!set) return;
 
-    Step("Get Set Class");
+
     jclass setClass = env->GetObjectClass(set);
     if (!setClass) return;
 
-    Step("Get toArray Method");
+
     jmethodID toArrayMid = env->GetMethodID(setClass, "toArray", "()[Ljava/lang/Object;");
     if (!toArrayMid) return;
 
-    Step("Call toArray");
+
     jobjectArray threads = (jobjectArray)env->CallObjectMethod(set, toArrayMid);
     if (!threads) return;
 
-    Step("Loop threads");
+
     jobject classLoader = nullptr;
     int threadCount = env->GetArrayLength(threads);
 
@@ -69,7 +65,7 @@ void DupePoC::Run(JNIEnv* env, int gunSlot, int attachSlot) {
         env->DeleteLocalRef(nameStr);
 
         if (tName == "Render thread" || tName == "Client thread") {
-            Step("Found thread");
+
             classLoader = env->CallObjectMethod(t, getContextClassLoaderMid);
             env->DeleteLocalRef(t);
             break;
@@ -79,11 +75,11 @@ void DupePoC::Run(JNIEnv* env, int gunSlot, int attachSlot) {
 
     if (!classLoader) return;
 
-    Step("Find Class java.lang.Class");
+
     jclass classClass = env->FindClass("java/lang/Class");
     if (!classClass) { env->ExceptionClear(); return; }
 
-    Step("Get forName method");
+
     jmethodID forNameMid = env->GetStaticMethodID(classClass, "forName", "(Ljava/lang/String;ZLjava/lang/ClassLoader;)Ljava/lang/Class;");
     if (!forNameMid) return;
 
@@ -95,35 +91,31 @@ void DupePoC::Run(JNIEnv* env, int gunSlot, int attachSlot) {
         return c;
         };
 
-    Step("Load AttachmentType");
+
     jclass attachTypeClass = loadClass("com.tacz.guns.api.item.attachment.AttachmentType");
     if (!attachTypeClass) return;
 
-    Step("Get EXTENDED_MAG");
+
     jfieldID extMagField = env->GetStaticFieldID(attachTypeClass, "EXTENDED_MAG", "Lcom/tacz/guns/api/item/attachment/AttachmentType;");
     if (!extMagField) { env->ExceptionClear(); return; }
 
-    Step("Get EXTENDED_MAG object");
+
     jobject extMagEnum = env->GetStaticObjectField(attachTypeClass, extMagField);
     if (!extMagEnum) return;
 
-    Step("Load ClientMessageRefitGun");
     jclass msgClass = loadClass("com.tacz.guns.network.message.ClientMessageRefitGun");
     if (!msgClass) return;
 
-    Step("Get ctor");
     jmethodID ctor = env->GetMethodID(msgClass, "<init>", "(IILcom/tacz/guns/api/item/attachment/AttachmentType;)V");
     if (!ctor) { env->ExceptionClear(); return; }
 
-    Step("NewObject msgObj");
+
     jobject msgObj = env->NewObject(msgClass, ctor, attachSlot, gunSlot, extMagEnum);
     if (!msgObj) return;
 
-    Step("Load NetworkHandler");
     jclass netHandlerClass = loadClass("com.tacz.guns.network.NetworkHandler");
     if (!netHandlerClass) return;
 
-    Step("Get CHANNEL field");
     jfieldID chanField = env->GetStaticFieldID(netHandlerClass, "CHANNEL", "Lnet/minecraftforge/network/simple/SimpleChannel;");
     if (!chanField) {
         env->ExceptionClear();
@@ -131,15 +123,15 @@ void DupePoC::Run(JNIEnv* env, int gunSlot, int attachSlot) {
     }
     if (!chanField) { env->ExceptionClear(); return; }
 
-    Step("Get CHANNEL object");
+
     jobject channel = env->GetStaticObjectField(netHandlerClass, chanField);
     if (!channel) return;
 
-    Step("Get Channel Class");
+
     jclass chanClass = env->GetObjectClass(channel);
     if (!chanClass) return;
 
-    Step("Get sendToServer method");
+
     jmethodID sendMid = env->GetMethodID(chanClass, "sendToServer", "(Ljava/lang/Object;)V");
     if (!sendMid) {
         env->ExceptionClear();
@@ -147,12 +139,12 @@ void DupePoC::Run(JNIEnv* env, int gunSlot, int attachSlot) {
     }
     if (!sendMid) return;
 
-    Step("CallVoidMethod");
+
     env->CallVoidMethod(channel, sendMid, msgObj);
 
-    Step("Cleanup");
+
     env->DeleteLocalRef(msgObj);
     env->DeleteLocalRef(channel);
     env->DeleteLocalRef(classLoader);
-    Step("Termine");
+
 }
--- kaka/dllmain.cpp
+++ kaka/dllmain.cpp
@@ -8,19 +8,9 @@
 #include "Blink.h"
 #include "MinHook.h"
 
-// Détecteur de crash 💥
-LONG WINAPI CrashHandler(EXCEPTION_POINTERS* ExceptionInfo) {
-    std::ofstream log("kaka_crash_log.txt", std::ios::app);
-    log << "💥 AIE, CRASH DETECTE ! 💥\n";
-    log << "Code d'erreur : 0x" << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode << "\n";
-    log << "Adresse : 0x" << ExceptionInfo->ExceptionRecord->ExceptionAddress << "\n";
-    log.close();
-    return EXCEPTION_CONTINUE_SEARCH;
-}
 
 DWORD WINAPI MainThread(HMODULE hModule) {
-    // 0. Activer la protection anti-crash 🛡️
-    AddVectoredExceptionHandler(1, CrashHandler);
+
 
     // 1. Initialiser MinHook en premier 😎
     if (MH_Initialize() != MH_OK) {
@@ -54,7 +44,7 @@ DWORD WINAPI MainThread(HMODULE hModule) {
     // 3. Nettoyer les hooks proprement avant de quitter 😌
     MH_DisableHook(MH_ALL_HOOKS);
     MH_Uninitialize();
-    RemoveVectoredExceptionHandler(CrashHandler); // On retire le logger 🧹
+
 
     FreeLibraryAndExitThread(hModule, 0);
     return 0;
